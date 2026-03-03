24c81c391af8379b06e617cd65a8674346e756b7
JeremyTor17 <jeremytorxd@gmail.com>
2026-03-03 09:51:33 -0400

    Version Actualizada

--- server.js
+++ server.js
@@ -1,13 +1,47 @@
-const express = require('express');
-const path = require('path');
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.set("view engine", "ejs")
 app.use(express.static(path.join(__dirname, 'public')));
+app.set("views", path.join(__dirname, "views"))
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'views', 'index.html'));
+  res.render("index");
+});
+
+app.get('/about', (req, res) => {
+  res.render("about");
+});
+
+app.get('/service', (req, res) => {
+  res.render("service");
+});
+
+app.get('/team', (req, res) => {
+  res.render("team");
+});
+
+app.get('/portfolio', (req, res) => {
+  res.render("portfolio");
+});
+
+app.get('/blog', (req, res) => {
+  res.render("blog");
+});
+
+app.get('/single', (req, res) => {
+  res.render("single");
+});
+
+app.get('/contact', (req, res) => {
+  res.render("contact");
 });
 
 app.listen(PORT, () => {
